0e0cf1a882a82416cea8de05a7f47094d299bb4b
Jadicus <johnzpan@gmail.com>
2016-06-28 17:40:05 -0400

    Add initial project files

---
+++ consoleApp/consoleApp/DelegateLambda.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+namespace consoleApp
+{
+    class DelegateLambda
+    {
+        public DelegateLambda(string name, int age, decimal salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; } 
+    }
+
+    class EmployeeFilterByAge
+    {
+        int m_age;
+        public EmployeeFilterByAge(int age)
+        {
+            m_age = age;
+        }
+        public bool OlderThan(DelegateLambda employee)
+        {
+            return employee.Age > m_age;
+        }
+    }
+
+    class TestLambda
+    {
+        public static void Test()
+        {
+            List<DelegateLambda> employees = new List<DelegateLambda>();
+            employees.Add(new DelegateLambda("John", 33, 22000m));
+            employees.Add(new DelegateLambda("Eric", 42, 18000m));
+            employees.Add(new DelegateLambda("Michael", 32, 19500m));
+            int ageThreshold = 40;
+            Predicate<DelegateLambda> match = e => e.Age > ageThreshold;
+            ageThreshold = 30;
+            int index = employees.FindIndex(match);
+            Console.WriteLine(index);
+
+            Console.WriteLine(employees.FindIndex(e => e.Age > 40));
+        }
+    }
+
+    class TestAnonymousMethods
+    {
+        public static void Test()
+        {
+            List<DelegateLambda> employees = new List<DelegateLambda>();
+            employees.Add(new DelegateLambda("John", 33, 22000m));
+            employees.Add(new DelegateLambda("Eric", 42, 18000m));
+            employees.Add(new DelegateLambda("Michael", 33, 19500m));
+            EmployeeFilterByAge filterByAge = new EmployeeFilterByAge(40);
+            int index = employees.FindIndex(filterByAge.OlderThan);
+            Console.WriteLine(index);
+
+            Console.WriteLine(employees.FindIndex(delegate(DelegateLambda employee)
+            {
+                return employee.Age > 40;
+            }));
+        }
+    }
+}
---
+++ consoleApp/consoleApp/Event.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace consoleApp
+{
+    public class Button
+    {
+        public delegate void ClickHandler(object sender, EventArgs e);
+        public event ClickHandler Click;
+        protected void OnClick()
+        {
+            // This is for safe event invokation in multithread env. However, this can cause object to receive
+            // event even after unsubscribe from receiving it.
+            ClickHandler clickHandler = Click;
+            if (clickHandler != null)
+            {
+                clickHandler(this, EventArgs.Empty);
+            }
+        }
+        public void SimulateClick()
+        {
+            OnClick();
+        }
+
+    }
+
+    public class Button2
+    {
+        ConcurrentDictionary<object, EventHandler> m_delegateStore =
+        new ConcurrentDictionary<object, EventHandler>();
+        static object clickEventKey = new object();
+        public event EventHandler Click
+        {
+            add
+            {
+                m_delegateStore.AddOrUpdate(
+                clickEventKey,
+                value,
+                (key, oldValue) =>
+                (EventHandler)Delegate.Combine(oldValue, value));
+            }
+            remove
+            {
+                m_delegateStore.AddOrUpdate(
+                clickEventKey,
+                (EventHandler) null,
+                (key, oldValue) =>
+                (EventHandler)Delegate.Remove(oldValue, value));
+            }
+        }
+        protected void OnClick()
+        {
+            EventHandler handler;
+            if (m_delegateStore.TryGetValue(clickEventKey, out handler))
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+        public void SimulateClick()
+        {
+            OnClick();
+        }
+    }
+
+    class TestEvent
+    {
+        static public void ButtonHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button clicked");
+        }
+
+        static public void ButtonHandler2(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button Clicked 2");
+        }
+        public static void Test()
+        {
+            Button button = new Button();
+            button.Click -= ButtonHandler; // not null pointer exception!!!
+            button.Click += ButtonHandler;
+            button.SimulateClick();
+            button.Click += ButtonHandler2;
+            button.SimulateClick();
+
+            Button2 btn2 = new Button2();
+            btn2.Click += ButtonHandler;
+            btn2.Click += ButtonHandler2;
+            button.SimulateClick();
+            btn2.Click -= ButtonHandler;
+            button.SimulateClick();
+
+        }
+    }
+}
